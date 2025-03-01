da5aedbe0950e1937cf8e9f3dbe856fb6c299664
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2025-02-28 21:21:20 -0500

    Implemented info persistence between scenes
    The InfoBank script holds all of the info about the game that needs to be saved between scene changes. Added this script, updated the PlayerInventory script, and added the Shop scene.

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -1,31 +1,54 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInventory : MonoBehaviour
 {
 
+    // Inventory is represented as an array of integers.
+    // Each item in the game is assigned an integer ID, so the value at index ID is the number of that item the player has.
     public int[] inventory = {0, 0, 0, 0, 0};
+    public int money = 100;
+    public bool toggle = false;
 
     void Start()
     {
-        
+        // UICanvas.SetActive(UIIsActive);
     }
 
     void Update()
     {
-        
+        // Update the player's information in InfoBank
+        if (!toggle) {
+            Debug.Log("Putting player inventory into InfoBank inventory!");
+            toggle = true;
+        }
+        InfoBank.instance.playerInventory = inventory;
+        InfoBank.instance.playerMoney = money;
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            if (SceneManager.GetActiveScene().name == "Present") {
+                SceneManager.LoadScene(sceneName:"Not Present");
+            }
+            else {
+                SceneManager.LoadScene(sceneName:"Present");
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            SceneManager.LoadScene(sceneName:"Shop");
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-
-        // Debug.Log("Bup");
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         // Check if the other object is an item
         if (other.gameObject.tag == "Item") {
 
-            // Add the item to your inventory
+            // Add the item to your inventory, and remove it from the scene.
             Item item = other.gameObject.GetComponent<Item>();
             inventory[item.item_id]++;
-            
-            // Destroy the object
             Destroy(other.gameObject);
         }
     }
